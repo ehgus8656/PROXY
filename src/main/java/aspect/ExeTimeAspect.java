3a7e91fb8014bc20cd7e668d89ae11e52eb8c521
ehgus8656 <ehak159@gmail.com>
2020-02-11 18:53:13 +0900

    2020-02-11 야간2

--- src/main/java/aspect/ExeTimeAspect.java
+++ src/main/java/aspect/ExeTimeAspect.java
@@ -14,10 +14,11 @@ public class ExeTimeAspect {
 		}
 		finally {
 			long finish = System.nanoTime();
-			Signature sig = joinPoint.getSignature();
+			Signature sig = joinPoint.getSignature();//호출되는 메서드의 정보를 구한다.
 			System.out.printf("%s, %s(%s) 실행시간 : %d ns\n",
-					joinPoint.getTarget().getClass().getSimpleName(),
-					sig.getName(),Arrays.toString(joinPoint.getArgs()),
+					joinPoint.getTarget(/*대상 객체를 반환*/).getClass().getSimpleName(),
+					sig.getName(/*메서드의 이름반환*/),
+					Arrays.toString(joinPoint.getArgs()/*파라미터의 목록을 구함*/),
 					(finish - start));
 		}
 	}
